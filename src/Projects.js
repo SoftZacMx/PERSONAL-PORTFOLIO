4231ab64dd25aa80b4fd510867cdf79156562f7b
Brayan <brayan@MacBook-Air-de-Brayan.local>
2025-05-13 12:46:13 -0600

    Changing SIDGDEP url

--- src/Projects.js
+++ src/Projects.js
@@ -23,22 +23,11 @@ function ProjectsSection() {
         <div className="projects-section">
             <h2>Some of My Most Important Projects</h2>
             <div className="projects-container">
-                <div className="card">
-                    <h3>ERP-FRUTERIA</h3>
-                    <p>A sales management system for a fruit store.</p>
-                    <a href="https://erp-fruteria.up.railway.app" target="_blank" rel="noopener noreferrer">
-                        <button>See More</button>
-                    </a>
-                    <section className="credentials">
-                        <h4>Credentials</h4>
-                        <p><strong>User:</strong> 1111111111</p>
-                        <p><strong>Password:</strong> adminadmin</p>
-                    </section>
-                </div>
+
                 <div className="card">
                     <h3>SIDGDER</h3>
                     <p>Comprehensive management system for restaurants.</p>
-                    <a href="https://sidgder-frontend.up.railway.app" target="_blank" rel="noopener noreferrer">
+                    <a href="https://softzac-sidgder-demo.up.railway.app" target="_blank" rel="noopener noreferrer">
                         <button>See More</button>
                     </a>
                     <section className="credentials">
@@ -50,13 +39,13 @@ function ProjectsSection() {
                 <div className="card">
                     <h3>SIDGDEP</h3>
                     <p>Comprehensive system for gym management.</p>
-                    <a href="https://sidgdep-frontend.up.railway.app" target="_blank" rel="noopener noreferrer">
+                    <a href="https://softzac-academies-demo.up.railway.app" target="_blank" rel="noopener noreferrer">
                         <button>See More</button>
                     </a>
                     <section className="credentials">
                         <h4>Credentials</h4>
-                        <p><strong>User:</strong> example@gmail.com</p>
-                        <p><strong>Password:</strong> admin</p>
+                        <p><strong>User:</strong> admin@gmail.com</p>
+                        <p><strong>Password:</strong> adminadmin</p>
                     </section>
                 </div>
             </div>
